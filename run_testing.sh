b266c76a8085ae521df3a51e2bc37325de7323b7
Owen Melia <owenjmelia@gmail.com>
2021-03-19 13:57:24 -0500

    added testing script

---
+++ run_testing.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#SBATCH --job-name=test_smoke3_vel_buo3_f250
+#SBATCH --time=4:00:00
+#SBATCH --output=00_test_smoke3_vel_buo3_f250.out
+#SBATCH --error=00_test_smoke3_vel_buo3_f250.err
+#SBATCH --partition=general
+#SBATCH --mem-per-gpu=8G           # memory per GPU required by your script. This is NOT GPU memory
+#SBATCH --gpus=1            # total number of GPUs required: gpu-type:qty
+#SBATCH --gpus-per-task=1   # gpu-type:qty
+#SBATCH --cpus-per-task=1           # number of cpus (not hyperthreaded) per task
+
+echo "`date` Starting Job"
+echo "SLURM Info: Job name:${SLURM_JOB_NAME}"
+echo "    JOB ID: ${SLURM_JOB_ID}"
+echo "    Host list: ${SLURM_JOB_NODELIST}"
+
+
+source /opt/conda/bin/activate deep_fluids
+python main.py \
+--is_train=False \
+--load_path=MODEL_DIR \
+--test_batch_size=5 \
+--is_3d=True \
+--dataset=smoke3_vel5_buo3_f250 \
+--res_x=112 \
+--res_y=64 \
+--res_z=32 \
+--batch_size=4 \
+--num_worker=1
